59e77256aee6b22f0d993cf4c4f0477236589509
AzashiroTaha <tahaoffice17@gmail.com>
2025-06-28 05:36:59 -0400

    saturday morning

--- main.c
+++ main.c
@@ -1,72 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include"function.h"
-#include"menu.h"
+#include "function.h"
+#include "menu.h"
 
-int main(){
-   int choice1 = menu();
-   char adfile[] = "admin_file.txt", clfile[] = "client_file.txt", accfile[] = "account_file.txt"; 
+int main() {
+    int choice1;
+    char adfile[] = "admin_file.txt", clfile[] = "client_file.txt", accfile[] = "account_file.txt";
+    int running = 1;
 
-   system("clear");
-   switch (choice1)
-   {
-   case 1:
-    ADMIN ad;
-     printf(">>>>> Bienvenu dans votre espace Client <<<<<\n");
-     printf("---------------------------------------------\n");
-     system("read -p 'Press enter to continue'");
+    do {
+        choice1 = menu();
+        system("clear");
 
-
-     
-     //newAdmin("admin_file.txt",ad);
-     break;
-   case 2:
-          char login[30], passwd[30];
-          printf("----- SE CONNECTER A VOTRE COMPTE -----\n");
-          printf("Pseudo: \n");
-          //fgets(login, sizeof(login), stdin);
-          scanf("%30s", login);
-          printf("Mot de passe: \n");
-          fgets(passwd, sizeof(passwd), stdin);
-          //int response = adminlog("admin_file.txt", login, passwd);
-          scanf("%30s", passwd);
-          int response = adminlog("admin_file.txt",login,passwd);
-          switch (response)
-          {
-              // case 0:
-              //   printf("Pseudo ou Mot de passe incorrecte");
-              //   break;
-              case 1:
-                system("clear");
-                printf("> Bienvenu dans votre espace Administrateur <\n");
+        switch (choice1) { // Main menu
+            case 1: {
+                ADMIN ad;
+                printf(">>>>> Bienvenu dans votre espace Client <<<<<\n");
                 printf("---------------------------------------------\n");
-                system("read 1 -s -r -p 'Appuyez sur une touche pour continuer'");
+                system("read -p 'Press enter to continue'");
+                // newAdmin("admin_file.txt", ad);
+                // Process finished, back to main menu automatically
+                break;
+            }
+            case 2: {
+                char login[30], passwd[30];
+                printf("----- SE CONNECTER A VOTRE COMPTE -----\n");
+                printf("Pseudo: \n");
+                scanf("%30s", login);
+                printf("Mot de passe: \n");
+                scanf("%30s", passwd);
+
+                int response = adminlog("admin_file.txt", login, passwd);
+
+                switch (response) { // Admin authentication
+                    case 1:
+                        system("clear");
+                        printf("> Bienvenu dans votre espace Administrateur <\n");
+                        printf("---------------------------------------------\n");
+                        system("read -p 'Appuyez sur une touche pour continuer'");
+                        system("clear");
+                        // After finishing, go back to main menu automatically
+                        break;
+
+                    default:
+                        printf("---------------------------------------------\n");
+                        printf("Pseudo ou Mot de passe incorrecte\n");
+                        break;
+                }
+                break;
+            }
+            case 3: { //Suscription
                 system("clear");
-
-                break;
-              default:
+                printf(">>  Bienvenu dans votre espace Inscription <<\n");
                 printf("---------------------------------------------\n");
-                perror("Pseudo ou Mot de passe incorrecte");
+                system("read -p 'Appuyez sur une touche pour continuer'");
+                //system("clear");
+                int choice3 = suscribe();
+                // Process finished, back to main menu automatically
                 break;
-          }
-              int stat;
+            }
+            case 4: { // Option to exit
+                running = 0;
+                printf("Merci d'avoir utilisé notre programme. Au revoir!\n");
+                break;
+            }
+            default:
+                printf("Cette option n'est pas valide\n");
+                break;
+        }
 
-          
-     break;
-   case 3:
-          system("clear");
-          printf(">>  Bienvenu dans votre espace Inscription <<\n");
-          printf("---------------------------------------------\n");
-          system("read 1 -s -r -p 'Appuyez sur une touche pour continuer'");
-          system("clear");
-          int choice3 = suscribe();
-          break;
+    } while (running);
 
-   
-   default:
-          printf("Cette option n'est pas valide\n");
-            break;
-   }
-    
-
+    return 0;
 }
--- menu.c
+++ menu.c
@@ -5,23 +5,38 @@
 
 
 //Principal MENU
-int menu(){
-    int choice;
-
-    do
-    {
-        printf("\n|=========================================|\n");
-        printf("|[1]Connexion Client                      |\n");
-        printf("|[2]Connexion Administrateur              |\n");
-        printf("|[3]Inscription                           |\n");
-        printf("|=========================================|\n");
-        printf(">>>>>");
-        scanf("%d", &choice);
-        system("clear");
-        if (choice <1 || choice > 3)
-            printf("\n[X]Attention! Choix %d invalide", choice);
-        system("clear");
-    } while (choice <1 || choice > 3);
-    
-    return choice;
+int menu() {
+    int choix;
+    printf("|=========================================|\n");
+    printf("|[1] Connexion Client                     |\n");
+    printf("|[2] Connexion Administrateur             |\n");
+    printf("|[3] Inscription                          |\n");
+    printf("|[4] Quitter                              |\n");
+    printf("|=========================================|\n");
+    printf(">>>>> ");
+    scanf("%d", &choix);
+    return choix;
 }
+
+
+
+// int menu(){
+//     int choice;
+
+//     do
+//     {
+//         printf("\n|=========================================|\n");
+//         printf("|[1]Connexion Client                      |\n");
+//         printf("|[2]Connexion Administrateur              |\n");
+//         printf("|[3]Inscription                           |\n");
+//         printf("|=========================================|\n");
+//         printf(">>>>>");
+//         scanf("%d", &choice);
+//         system("clear");
+//         if (choice < 1 || choice > 4)
+//             printf("\n[X]Attention! Choix %d invalide", choice);
+//         system("clear");
+//     } while (choice <1 || choice > 3);
+    
+//     return choice;
+// }
